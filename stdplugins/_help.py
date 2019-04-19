731326b0229ad3d6e6dad36556f00ea8aa4c773b
Akhil Dadhania <30388255+Akhildadhania@users.noreply.github.com>
2019-04-19 11:46:17 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -78,16 +78,7 @@ Telethon : {}```
         sys.version,
         __version__,len(borg._plugins),uptime_string,cpu,d.percent)
     
-    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
-    if tgbotusername is not None:
-        results = await borg.inline_query(tgbotusername, help_string)
-        message = await results[0].click(
-            event.chat_id,
-            reply_to=event.reply_to_msg_id,
-            hide_via=True
-        )
-        await event.delete()
-    else:
+    
         await event.edit(help_string,link_preview=True)
 
 
